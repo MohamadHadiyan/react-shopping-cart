f62e23fd68ae40be2956d19773744cfafbd4cacb
MohamadHadiyan <Hadiyanmohamad131@gmail.com>
2020-11-09 17:25:53 +0330

    create product component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 // feature 1
 // test 1
 import React from 'react';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>Products List</main>
-      <footer>
-        All Right is reserved.
-      </footer>
-    </div>
-  );
+//import costom component
+import data from './data.json'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="grid-container">
+
+        {/* ====================== Start header area ====================== */}
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        {/* ====================== End header area ====================== */}
+
+
+        {/* ====================== Start Main area ====================== */}
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+              Cart Items
+            </div>
+          </div>
+        </main>
+        {/* ====================== End Main area ====================== */}
+
+
+        {/* ====================== Start footer area ====================== */}
+        <footer>
+          All Right is reserved.
+        </footer>
+        {/* ====================== End footer area ====================== */}
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Util.js
@@ -0,0 +1,3 @@
+export default function FormatCurrency(num) {
+    return "$" + Number(num.toFixed(1)).toLocaleString() + " ";
+}
